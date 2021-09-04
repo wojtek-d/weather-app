5257fefb2b717ef3da17e8f33262bf73f97458df
wojtek-d <5qZQhi1YYfX9ZRzL01@gmail.com>
2021-09-04 14:27:14 +0200

    Delete old api key

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const button = document.querySelector("#submit-button");
 const inputTextBox = document.querySelector("#placeholder-text-box");
 
-const API_KEY = "1bdeb98a1a03ba728af543c82ca9cf9e";
+const API_KEY = "your-api-key-here";
 async function run(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
   const response = await fetch(url);
