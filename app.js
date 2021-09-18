7e227c7d42f3736deb085b565c2d7ffd77fc8015
wojtek-d <5qZQhi1YYfX9ZRzL01@gmail.com>
2021-09-18 21:56:57 +0200

    Added an option to display the weather directly on the site.

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const button = document.querySelector("#submit-button");
 const inputTextBox = document.querySelector("#placeholder-text-box");
+const API_KEY = "Enter your API key here";
+const countryDOM = document.querySelector("#country");
+const cityDOM = document.querySelector("#city");
+const temperatureDOM = document.querySelector("#temperature");
+const descriptionDOM = document.querySelector("#description");
+const flagDOM = document.querySelector("#flag-icon");
+const iconDOM = document.querySelector("#weather-icon");
 
-const API_KEY = "your-api-key-here";
 async function run(city) {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-  const response = await fetch(url);
-  const weatherData = await response.json();
+  const openWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+  const restCountriesURL = `https://restcountries.eu/rest/v2/alpha/`;
+  const responseOpenWeather = await fetch(openWeatherURL);
+  const weatherData = await responseOpenWeather.json();
+  const responseCountries = await fetch(
+    `${restCountriesURL}${weatherData.sys.country}`
+  );
+  const countryData = await responseCountries.json();
+  console.log(countryData);
+  let { icon, description } = weatherData.weather[0];
+  temperatureDOM.innerText = weatherData.main.temp + "°C";
+  countryDOM.innerText = countryData.name;
+  cityDOM.innerText = weatherData.name;
+  description = description.charAt(0).toUpperCase() + description.slice(1);
+  descriptionDOM.innerText = description;
+  flagDOM.src = countryData.flag;
+  iconDOM.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
   console.log(weatherData);
 }
 
 button.addEventListener("click", (event) => {
   event.preventDefault();
   if (inputTextBox.value === "") {
-    console.log("Search for a valid city");
+    alert("Search for a valid city");
     return;
   }
-  const city = inputTextBox.value;
-  run(city);
+  run(inputTextBox.value);
 });
